9d4b0f90b890bd6d527d448a6cee6f1d4651039e
Jeff T. Polczynski <jefftp@jefftp.com>
2025-01-30 21:45:59 -0600

    Move configuration options to "options.conf" file. Switch from fstab entry to systemd unit files for mounting SMB fileshare.

--- rustic/setup-rustic.sh
+++ rustic/setup-rustic.sh
@@ -10,21 +10,14 @@ if ! [ $(id -u) = 0 ]; then
   exit 1
 fi
 
-# Specify the SMB configuration here
-SMB_USERNAME='backup-user'
-SMB_PASSWORD='backup-password'
-SMB_DOMAIN='WORKGROUP'
-SMB_PATH='//nas/share'
-
-# ----------------------------------------- #
-# --- No changes needed below this line --- #
-# ----------------------------------------- #
+# Load options
+. ./options.conf
 
 # Set permissions for new files so they are only accessible by root
 umask 077
 
 # Create config and repo mount point directories
-mkdir /etc/rustic /mnt/backup
+mkdir /etc/rustic "$BACKUP_MOUNTPOINT"
 
 # Copy the Rustic configuration profile and set permissions
 cp rustic.toml /etc/rustic/
@@ -39,10 +32,44 @@ _EOF_
 # Create a random 32 character password for the backup repo password
 cat /dev/urandom | tr -dc [:alnum:] | head -c32 > /etc/rustic/repo-credentials
 
-# Update FSTAB with entry for /mnt/backup
-cat >> /etc/fstab <<_EOF_
-# /mnt/backup
-${SMB_PATH} /mnt/backup cifs rw,credentials=/etc/rustic/smb-credentials,vers=3,nosuid,nodev,noexec,noatime,file_mode=0600,dir_mode=0700,iocharset=utf8,x-system.automount,x-systemd.mount-timeout=30,x-systemd.idle-timeout=300,_netdev 0 0
+# Convert $BACKUP_MOUNTPATH to BACKUP_SYSTEMD_UNIT
+if [ -z "$BACKUP_MOUNTPATH" ] || [ "${BACKUP_MOUNTPATH#/}" = "$BACKUP_MOUNTPATH"]; then
+  echo "ERROR: BACKUP_MOUNTPATH must be an absolute path." >&2
+  exit 1
+fi
+
+# Remove the leading / from the mount path
+BACKUP_SYSTEMD_UNIT="${BACKUP_MOUNTPATH#/}"
+
+# Convert / to -
+BACKUP_SYSTEMD_UNIT=$(echo "$BACKUP_SYSTEMD_UNIT" | tr '/' '-')
+
+# Create .mount file for backup repo
+cat > "/etc/systemd/system/${BACKUP_SYSTEMD_UNIT}.mount" <<_EOF_
+[Unit]
+Description=Backup Repository
+After=network-online.target
+Wants=network-online.target
+
+[Mount]
+What=${SMB_PATH}
+Where=${BACKUP_MOUNTPOINT}
+Type=cifs
+Options=credentials=/etc/rustic/smb-credentials,vers=3,nosuid,nodev,noexec,noatime,file_mode=0600,dir_mode=0700,iocharset=utf8
+TimeoutSec=30
+
+[Install]
+WantedBy=multi-user.target
+_EOF_
+
+# Create .automount file for backup repo
+cat > "/etc/systemd/system/${BACKUP_SYSTEMD_UNIT}.automount" <<_EOF_
+[Unit]
+Description=Automount Backup Repository
+
+[Automount]
+Where=${BACKUP_MOUNTPOINT}
+TimeoutIdleSec=5min
 _EOF_
 
 # Copy service and timer unit files into place and set permissions
